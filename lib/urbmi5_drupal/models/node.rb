b1792ab8c4618dbf07e55b18d84a18a130b7c441
Jim Gummeson <jim@jim.techmission>
2012-12-07 13:41:14 -0500

    -fixed self.table_name= statements

--- lib/urbmi5_drupal/models/node.rb
+++ lib/urbmi5_drupal/models/node.rb
@@ -3,7 +3,7 @@ module Urbmi5Drupal
   class Node < ActiveRecord::Base
     include SafeAttributes::Base
     include ChrisvolModelage::Urbmi5Drupal::Connection
-    self.table_name "um_node"
+    self.table_name = "um_node"
 
     has_many :location_instances, :class_name => "Urbmi5Drupal::LocationInstance", :primary_key => "nid", :foreign_key => "nid"
     has_many :locations, :through =>  :location_instances
--- lib/urbmi5_drupal/models/session.rb
+++ lib/urbmi5_drupal/models/session.rb
@@ -1,7 +1,7 @@
 module Urbmi5Drupal
   class Session < ActiveRecord::Base
     include ChrisvolModelage::Urbmi5Drupal::Connection
-    self.table_name 'voleng_sessions'
+    self.table_name = 'voleng_sessions'
     belongs_to :user, :class_name => "Urbmi5Drupal::User", :primary_key => "uid", :foreign_key => "uid"
   end
 end
