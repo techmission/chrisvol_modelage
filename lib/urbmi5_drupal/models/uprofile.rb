6d8b845d34c425af10b4516840ea8bbbc2dd2475
unknown <Jim Gummeson@OPTIPLEX360-64.techmission.local>
2013-01-16 17:18:29 -0500

    -added validation on uprofile for birth year

--- lib/urbmi5_drupal/models/uprofile.rb
+++ lib/urbmi5_drupal/models/uprofile.rb
@@ -8,7 +8,12 @@ module Urbmi5Drupal
     include ChrisvolModelage::Urbmi5Drupal::ActsAsNode
 
     validates_presence_of :field_first_name_value, :field_last_name_value
-
+	validates_presence_of :field_site_use_type_value, :in => ["Both", "Church", "Organization", "Volunteer"]
+	validates :field_birth_year_value, 
+	          :presence     => true, 
+			  :numericality => {:only_integer => true, :greater_than => 999, :less_than => 10000},
+			  :if           => :existing_individual?
+	
 	def location_instance
 	  @location_instance ||= (self.node.location_instances.first || self.node.location_instances.new(:vid => self.node.vid))
 	end
@@ -16,5 +21,15 @@ module Urbmi5Drupal
     def location
 	  @location = (location_instance.location || location_instance.build_location)
     end
+	
+	def existing_individual?
+	  !new_record? and individual?
+	end
+	
+	def individual?
+	  case field_site_use_type_value
+	  when "Both", "Individual" then true
+	  else false
+	end
   end
 end
