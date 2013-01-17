11d338aa285fe13568c7f298a467147b0c3868d9
unknown <Jim Gummeson@OPTIPLEX360-64.techmission.local>
2013-01-16 19:27:27 -0500

    -added validations to locations associated with individual profiles

--- lib/urbmi5_drupal/models/location.rb
+++ lib/urbmi5_drupal/models/location.rb
@@ -4,5 +4,17 @@ module Urbmi5Drupal
     self.table_name = "um_location"
     self.primary_key = "lid"
 	has_many :phones, :class_name => "Urbmi5Drupal::Phone", :primary_key => :lid, :foreign_key => :lid
+	has_many :location_instances, :class_name => "Urbmi5Drupal::LocationInstance", :primary_key => :lid, :foreign_key => :lid
+	
+	validates_inclusion_of :country, :in => Techmi5Voleng::Country.find(:all).map{|c| c.iso}, :if => :belongs_to_individual?
+	validates_presence_of :postal_code, :if => :belongs_to_individual?
+	
+	def belongs_to_individual?
+	  location_instaces.
+	  map{|li| Urbmi5Drupal::Uprofile.find_by_nid(li.nid)}.
+	  compact.
+	  select{|up| up.individual?}.
+	  length > 0
+	end
   end
 end
