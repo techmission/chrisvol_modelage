c273506a11f81c893133775d6640b9813fd847f2
unknown <Jim Gummeson@OPTIPLEX360-64.techmission.local>
2013-01-16 19:43:24 -0500

    -fixed a couple bugs in my last commit

--- lib/urbmi5_drupal/models/location.rb
+++ lib/urbmi5_drupal/models/location.rb
@@ -9,10 +9,10 @@ module Urbmi5Drupal
 	has_many :location_instances, :class_name => "Urbmi5Drupal::LocationInstance", :primary_key => :lid, :foreign_key => :lid
 	
 	validates_inclusion_of :country, :in => Techmi5Voleng::Country.find(:all).map{|c| c.iso}, :if => :belongs_to_individual?
-	validates_presence_of :postal_code, :if => :belongs_to_individual?
+	validates_presence_of :postal_code, :if => Proc.new{|l| l.belongs_to_individual? and l.country.upcase == "US"}
 	
 	def belongs_to_individual?
-	  location_instaces.
+	  location_instances.
 	  map{|li| Urbmi5Drupal::Uprofile.find_by_nid(li.nid)}.
 	  compact.
 	  select{|up| up.individual?}.
