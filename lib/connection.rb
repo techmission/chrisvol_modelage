3c7b653d34d12d0d4e3da4b49a7fcb8802873fa6
unknown <Jim Gummeson@OPTIPLEX360-64.techmission.local>
2013-01-17 14:07:55 -0500

    -added debug printout

--- lib/connection.rb
+++ lib/connection.rb
@@ -2,6 +2,7 @@ module ChrisvolModelage
   def self.db_connect(model, yml_key)
     yml_key = yml_key.to_s
     yml_key += "_#{ENV['RAILS_ENV']}" if ENV['RAILS_ENV']
+	puts yml_key
     model.establish_connection(YAML.load(File.read('config/database.yml'))[yml_key])
   end
 end
