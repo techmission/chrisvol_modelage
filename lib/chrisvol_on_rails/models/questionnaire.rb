a4799fe67b7e287db1bcefff6bb4141ca3077942
unknown <Jim Gummeson@OPTIPLEX360-64.techmission.local>
2013-05-08 17:44:44 -0400

    -fixed broken association

--- lib/chrisvol_on_rails/models/questionnaire.rb
+++ lib/chrisvol_on_rails/models/questionnaire.rb
@@ -2,7 +2,7 @@ class Questionnaire < ActiveRecord::Base
   include ChrisvolModelage::ChrisvolOnRails::Connection
 
   has_many :questions, :primary_key => "id", :foreign_key => "questionnaire_id", :order => "position ASC"
-  belongs_to :organization, :class_name => "Urbmi5Drupal::Organization", :foreign_key => "nid", :primary_key => "organization_nid"
+  belongs_to :organization, :class_name => "Urbmi5Drupal::Organization", :primary_key => "nid", :foreign_key => "organization_nid"
   
   validates_presence_of :title
   
