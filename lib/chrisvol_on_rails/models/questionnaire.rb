525d7ea8c98b67bc9ad8e8d1071586b4c42491df
unknown <Jim Gummeson@OPTIPLEX360-64.techmission.local>
2013-05-09 10:37:13 -0400

    -added opportunities_questionnaires association to questionnaire model

--- lib/chrisvol_on_rails/models/questionnaire.rb
+++ lib/chrisvol_on_rails/models/questionnaire.rb
@@ -2,8 +2,9 @@ class Questionnaire < ActiveRecord::Base
   include ChrisvolModelage::ChrisvolOnRails::Connection
 
   has_many :questions, :primary_key => "id", :foreign_key => "questionnaire_id", :order => "position ASC"
+  has_many :opportunities_questionnaires
   belongs_to :organization, :class_name => "Urbmi5Drupal::Organization", :primary_key => "nid", :foreign_key => "organization_nid"
-  
+ 
   validates_presence_of :title
   
   def opportunity
